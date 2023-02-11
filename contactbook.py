35d82f822210a1589c11b84d8d03723a3ba503c6
DeniseIra02 <irajubilo@gmail.com>
2023-02-11 14:34:08 +0800

    start setting up the input area of the interface, using entryclass. first for the name input.

--- contactbook.py
+++ contactbook.py
@@ -24,7 +24,14 @@ frame_table = Frame(window, width = 500, height = 100, bg = co2)
 frame_table.grid(row = 2, column = 0, columnspan = 2, padx = 0, pady = 1)
 
 #frame_up style/widgets
-app_name = Label(frame_up, text = "Contactbook", height = 1, font = ('Ivy 17 bold'), bg = co2, fg = co0)
+app_name = Label(frame_up, text = "Contactbook", height = 1, font = ('Verdana 17 bold'), bg = co2, fg = co0)
 app_name.place(x = 5, y = 5)
+
+#frame_down style/widgets
+l_name = Label(frame_down, text = "Name *", width = 20, height = 1, font = ('Ivy 10'), bg = co0, anchor = NW)
+l_name.place(x = 10, y = 20)
+
+e_name = Entry(frame_down, width = 25, justify = "left", highlightthickness = 1, relief = "solid")
+e_name.place(x = 80, y = 20)
 
 window.mainloop()
