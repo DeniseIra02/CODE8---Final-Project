f6c4768981035a3134a4833dc6f434520fbbc298
DeniseIra02 <irajubilo@gmail.com>
2023-02-11 14:00:11 +0800

    adding the structure of the window. Uses class Frame and grid

--- contactbook.py
+++ contactbook.py
@@ -4,12 +4,22 @@ from tkinter import ttk
 #colors
 co0 = "#ffffff"
 co1 = "#000000"
-co9 = "#4456F0"
+co2 = "#4456F0"
 
 window = Tk()
 window.title("Contact Book")
 window.geometry('485x450')
 window.configure(background = co0)
 window.resizable(width = False, height = False)
+
+#
+frame_up = Frame(window, width=500, height=50, bg=co2)
+frame_up.grid(row=0, column=0, padx=0, pady=1)
+
+frame_down = Frame(window, width=500, height=150, bg=co0)
+frame_down.grid(row=1, column=0, padx=0, pady=1)
+
+frame_table = Frame(window, width=500, height=100, bg=co2)
+frame_table.grid(row=2, column=0, columnspan=2, padx=0, pady=1)
 
 window.mainloop()
