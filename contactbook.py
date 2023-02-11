03341e367817f0040c14a51d80eaed2501293f57
DeniseIra02 <irajubilo@gmail.com>
2023-02-11 13:46:09 +0800

    Contactbook. Added the module that will be used in the project. Set colors into variables and added the layout of the window.

---
+++ contactbook.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+#colors
+co0 = "#ffffff"
+co1 = "#000000"
+co9 = "#4456F0"
+
+window = Tk()
+window.title("Contact Book")
+window.geometry('485x450')
+window.configure(background = co0)
+window.resizable(width = False, height = False)
+
+window.mainloop()
