d03cccd27e89584efb16064012ac9d2cc85e58cd
DeniseIra02 <irajubilo@gmail.com>
2023-02-11 18:58:58 +0800

    the function view is to read all the data row by row in the csv file.

--- views.py
+++ views.py
@@ -1,9 +1,22 @@
 import sys
 import csv
 
+#add a data
 def add(i):
     with open('data.csv', 'a+', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(i)
-        
+
+#dummydata        
 add(['Anonymous', 'M', '54321', 'data@gmail.com'])
+
+#view data/show data
+def view():
+    data = []
+    with open('data.csv') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            data.append(row)
+    print(data)
+
+view()
